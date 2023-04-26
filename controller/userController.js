96bc7d589cdbe45f2f434452a588381fca97d437
donib-irakihda <donib.irakihda@gmail.com>
2023-04-26 11:50:13 +0545

    change: Used express validators as middleware and removed from the controller

--- controller/userController.js
+++ controller/userController.js
@@ -9,8 +9,6 @@ require("dotenv").config();
 
 const SECRET_JWT = process.env.SECRET_JWT;
 
-const { body, validationResult } = require("express-validator");
-
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
@@ -47,31 +45,6 @@ const registerUser = async (req, res) => {
       token: tempToken,
     });
 
-    user.save().then().catch();
-
-    //Validation
-    await body("fullName")
-      .notEmpty()
-      .withMessage("Full Name is required1")
-      .isLength({ min: 2 })
-      .withMessage("Name should be more than 2 characters")
-      .run(req);
-
-    await body("email")
-      .isEmail()
-      .withMessage("Please enter valid email")
-      .run(req);
-
-    await body("password")
-      .isLength({ min: 6 })
-      .withMessage("Password must be greater than 6 characters")
-      .run(req);
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
     //data from req
     const {
       fullName,
@@ -155,21 +128,9 @@ const emailVerify = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  await body("email").isEmail().withMessage("Invalid Email").run(req);
-  await body("password")
-    .notEmpty()
-    .withMessage("Password is required")
-    .run(req);
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { email, password } = req.body;
-
   try {
+    const { email, password } = req.body;
+
     const existingUser = await userModel.findOne({ email: email });
 
     if (!existingUser) {
@@ -204,21 +165,5 @@ const loginUser = async (req, res) => {
     res.status(500).json({ error: "Login failed" });
   }
 };
-
-// logoutUser
-// const logoutUser = async (req, res) => {
-//   try {
-//     let token = req.headers.authorization;
-//     if (token) {
-//       token = token.split(" ")[1];
-//       let user = jwt.verify(token, SECRET_JWT);
-
-//       res.status(200).json({ message: "Logout success", newToken: newToken });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Logout failed" });
-//   }
-// };
 
 module.exports = { registerUser, loginUser, emailVerify };
