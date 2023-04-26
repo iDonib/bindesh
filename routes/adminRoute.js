eb711d98bf6c42509aa67e6616e3bd87ff6f6a52
Sandesh1998 <sandeshpoudel144@gmail.com>
2023-04-26 15:01:31 +0545

    Add: added forget and reset password

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const adminRouter = express.Router();
-const session = require("express-session");
+
 const { adminLogin, onlyAdmin } = require("../controller/adminController");
-const { isAdmin, isLoggedOut, isLoggedIn } = require("../middleware/auth");
+const { isAdmin } = require("../middleware/auth");
 
 adminRouter.post("/login", adminLogin);
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,18 +3,29 @@ const {
   registerUser,
   loginUser,
   emailVerify,
+  forgotPassword,
+  resetPassword
 } = require("../controller/userController");
 
-const { isLoggedIn, isLoggedOut, isAdmin } = require("../middleware/auth");
+const { isLoggedIn, isAdmin } = require("../middleware/auth");
 
 const userRoute = express.Router();
+
 // register
 userRoute.post("/register", registerUser);
+
 // login
 userRoute.post("/login", loginUser);
 // logout
 // userRoute.post("/logout", isLoggedIn, logoutUser);
+
 // verify email
 userRoute.get("/verifyEmail/:token", emailVerify);
+
+// forgot password
+userRoute.post("/forgotPassword", forgotPassword);
+
+// reset password
+userRoute.post("/reset-password/:token", resetPassword);
 
 module.exports = userRoute;
