f62be6433b7ec4d2a2e981c0b0e2e7be5d5d0110
donib-irakihda <donib.irakihda@gmail.com>
2023-04-28 15:27:22 +0545

    add: added org schema

---
+++ model/organization.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const User = require("../model/user");
+
+const organizationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      min: 8,
+    },
+    address: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    id: true,
+  }
+);
+
+const Organization = mongoose.model("Organization", organizationSchema);
+module.exports = Organization;
