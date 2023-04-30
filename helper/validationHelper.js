ee811b9b1ab9f800aadd431de249921f2ef7792f
donib-irakihda <donib.irakihda@gmail.com>
2023-04-30 15:54:41 +0545

    fix: did somefixes

--- helper/validationHelper.js
+++ helper/validationHelper.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const validateReq = function (req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(200).json({ errors: errors.array() });
+    return res.status(500).json({ errors: errors.array() });
   }
   next();
 };
