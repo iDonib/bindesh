a803d912b63285b913f8e27c3aa4a85c4d21c4de
donib-irakihda <donib.irakihda@gmail.com>
2023-05-08 17:50:20 +0545

    fix: fixed html of emai verifiaction

--- controller/userController.js
+++ controller/userController.js
@@ -34,7 +34,14 @@ const registerUser = async (req, res) => {
         from: "Bindesh",
         to: "james1415161718s@gmail.com",
         subject: "Verify your email",
-        html: `<h1>Hi ${name}</h1><br><p>Click this link to verify your email <a href="localhost:5000/api/user/emailVerify?id=${userId}">Click here</a></p>`,
+        html: `
+        <h1>Hi ${name},</h1>
+        <p>Thank you for registering on OrgFeeder. Please click on the link below to verify your email address:</p>
+        <a href="http://localhost:8000/orgFeeder/api/user/emailVerify?id=${userId}">Verify Email</a>
+        <p>If you did not register on our website, please ignore this email.</p>
+        <p>Thank you,</p>
+        <p>OrgFeeder Team</p>
+      `,
       };
 
       console.log(mailOptions.html);
