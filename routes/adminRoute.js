c5d762e2655109445cefed066467b7faef3cae65
Sandesh1998 <sandeshpoudel144@gmail.com>
2023-05-08 12:15:12 +0545

    feat: get all user in admin pannel

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const adminRouter = express.Router();
 
-const { adminLogin, onlyAdmin } = require("../controller/adminController");
+const {
+  adminLogin,
+  onlyAdmin,
+  getAllUsers,
+  deleteUser,
+} = require("../controller/adminController");
 const { isAdmin } = require("../middleware/auth");
+const { isLoggedIn } = require("../middleware/auth");
 const { validateLoginUser } = require("../validators/userValidator");
 
 adminRouter.post("/login", validateLoginUser, adminLogin);
 
 adminRouter.get("/adminOnly", isAdmin, onlyAdmin);
+
+adminRouter.get("/allUsers", isLoggedIn, isAdmin, getAllUsers);
+
+adminRouter.delete("/deleteUser/:id", isLoggedIn, isAdmin, deleteUser);
 
 module.exports = adminRouter;
