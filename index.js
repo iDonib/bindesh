8c3f32a04866e8612b479537756f9412b01b0129
Binod Adhikari <donib.irakihda@gmail.com>
2023-05-16 11:21:02 +0545

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ const corsOption = {
   ],
 };
 app.use(require("cors")(corsOption));
+
+/cors policy
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (corsOption.origin.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.json({});
+  }
+  next();
+});
+
 // Routes
 const routers = require("./main.route");
 app.use("/orgFeeder/api", routers);
